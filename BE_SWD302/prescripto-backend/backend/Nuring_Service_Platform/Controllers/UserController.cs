f5876d51f70125c81a54258c98b5fd5b5655d02d
nghiahoanglamfer <nghiaduong121@gmail.com>
2025-02-24 08:40:31 +0700

    add function

---
+++ BE_SWD302/prescripto-backend/backend/Nuring_Service_Platform/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Nuring_Service_Platform.Services;
+using Repositories.Dtos.BackgroundDoctorDto;
+using Repositories.Entities;
+using Services.Interfaces;
+
+namespace Nuring_Service_Platform.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IClaimsService _claimsService;
+        public UserController(IUserService userService, IClaimsService claimsService)
+        {
+            _userService = userService;
+            _claimsService = claimsService;
+        }
+
+        //[HttpPost("profile")]
+        //public async Task<IActionResult> CreateBackgroundDoctor(CreateBackgroundDto createBackgroundDto)
+        //{
+        //    return Ok(await _userService.CreateBackgroundDoctor(createBackgroundDto));
+        //}
+
+        //[HttpPut("doctor/profile")]
+        //public async Task<IActionResult> UpdateBackgroundDoctor(UpdateBackgroundDoctorDto updateBackgroundDoctorDto)
+        //{
+        //    var userId = _claimsService.CurrentUserId;
+        //    if (userId == Guid.Empty)
+        //        return BadRequest("userId is not found");
+        //    return Ok(await _userService.UpdateBackgroundDoctor(userId, updateBackgroundDoctorDto));
+        //}
+
+        //[HttpGet("doctor/all")]
+        //public async Task<IActionResult> GetAllBackgroundDoctor()
+        //{
+        //    return Ok(await _userService.GetAllBackgroundDoctor());
+        //}
+    }
+}
---
+++ BE_SWD302/prescripto-backend/backend/Repositories/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly NuringServiceDbContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IDoctorRepository _doctorRepository;
+        public UnitOfWork(NuringServiceDbContext context ,IUserRepository userRepository, IDoctorRepository doctorRepository) 
+        { 
+            _context = context;
+            _userRepository = userRepository;
+            _doctorRepository = doctorRepository;
+        }
+        public IUserRepository UserRepository => _userRepository;
+
+        public IDoctorRepository DoctorRepository => _doctorRepository;
+
+        public async Task<int> SaveChangeAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
